d3ecdc429ac2a126e370e3102481e3f10655bc98
vatsmayank <vatsmayank2004@gmail.com>
2017-06-19 09:40:01 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
-
-var express = require('express'),
-    path = require('path'),
-    //configure = require('./server/configure_server'),
-    //fs = require('fs'),
-    app = express();
-    //https = require('https'),
-    //http = require('http');
-    //mongoose = require('mongoose');
-
-//const express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const restService = express();
@@ -21,69 +11,14 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.expenseCategory ? req.body.result.parameters.expenseCategory : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
 });
-
-restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "slack": slack_message
-        }
-    });
-});
-
-
 
 
 restService.listen((process.env.PORT || 8000), function() {
